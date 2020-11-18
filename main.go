0b5e71758fee6de09e87afe07f6b33e7335aa80e
amidg4x4 <amidg4x4dima@gmail.com>
2020-11-17 23:45:21 -0500

    default wolf can be generated

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	//wolfTest "Wolf/Wolf"
 
-	"image"
-	_ "image/jpeg"
+	//required to read image from file
+
+	"image/jpeg"
+	"os"
+
+	"github.com/fogleman/gg" //needed to draw text on image
 )
 
 //WILL BE REMOVED LATER INTO A SEPARATE FILE
@@ -31,5 +35,52 @@ func main() {
 	fmt.Println(wolf.name)
 	fmt.Println(wolf.wisdom)
 
-	img := image.Image{}
+	// Read image from file that already exists
+	wolfImage, err := os.Open("Source/wolf1.jpeg")
+	if err != nil {
+		// Handle error
+	}
+	defer wolfImage.Close()
+
+	// directly decode image
+	loadedWolfImage, err := jpeg.Decode(wolfImage)
+	if err != nil {
+		// Handle error
+	}
+	imageRect := loadedWolfImage.Bounds()
+	wolfImgWidth := imageRect.Dx()
+	wolfImgHeight := imageRect.Dy()
+	fmt.Print(wolfImgWidth)
+	fmt.Print(" by ")
+	fmt.Println(wolfImgHeight)
+
+	//apply text
+	const fontSize = 48
+	imagePath := "./wolfMeme.jpeg"
+
+	m := gg.NewContext(wolfImgWidth, wolfImgHeight)
+	m.DrawImage(loadedWolfImage, 0, 0)
+	m.LoadFontFace("/Library/Fonts/Impact.ttf", fontSize)
+
+	// Apply black stroke
+	m.SetHexColor("#000")
+	strokeSize := 6
+	for dy := -strokeSize; dy <= strokeSize; dy++ {
+		for dx := -strokeSize; dx <= strokeSize; dx++ {
+			// give it rounded corners
+			if dx*dx+dy*dy >= strokeSize*strokeSize {
+				continue
+			}
+			x := float64(wolfImgWidth/2 + dx)
+			y := float64(wolfImgHeight - fontSize + dy)
+			m.DrawStringAnchored(wolf.wisdom, x, y, 0.5, 0.5)
+		}
+	}
+
+	// Apply white fill
+	m.SetHexColor("#FFF")
+	m.DrawStringAnchored(wolf.wisdom, float64(wolfImgWidth)/2, float64(wolfImgHeight)-fontSize, 0.5, 0.5)
+	m.SavePNG(imagePath)
+
+	fmt.Printf("Saved to %s\n", imagePath)
 }

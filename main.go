9718d803525dc860a142bf983ec6ed6902ca6865
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-02-10 22:57:19 -0800

    fix line counting

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	//wolfTest "Wolf/Wolf"
 
 	//required to read image from file
-
 	"image"
 	"image/jpeg"
 	_ "image/png"
@@ -16,15 +15,19 @@ import (
 	"os"
 	"time"
 
+    "strings"
+
 	"bufio"
 
 	"github.com/fogleman/gg" //needed to draw text on image
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 /////////////////////////////////////////////
 //GLOBAL VARIABLES
 var (
-	pathToWolfImagesFolder  = "./Source/"
+	pathToWolfImagesFolder  = "./ImgSource/"
 	numberOfAvailableImages = 0
 	wolfTemplateNames       [999]string //NO IDEA HOW MUCH RAM THIS TAKES, PROBABLY COUPLE MBs, specified maximum  number of images as long as there is enough RAM
 	userWisdom              = "Wolf is stronger than lion but buys beer"
@@ -58,7 +61,7 @@ func newWolf(newWolfName string, newWolfWisdom string) *Wolf {
 func readAndDecodeImage() (image.Image, int, int) { //returns regenerated jpegs and its sizes
 	rand.Seed(time.Now().UnixNano())
 	var imageChosen = rand.Intn(numberOfAvailableImages) //choose random image between 0 and max image
-	var imagePath = "Source/" + wolfTemplateNames[imageChosen]
+	var imagePath = "ImgSource/" + wolfTemplateNames[imageChosen]
 	//fmt.Println(imagePath)
 
 	// Read image from file that already exists
@@ -87,12 +90,21 @@ func readAndDecodeImage() (image.Image, int, int) { //returns regenerated jpegs 
 
 func generateWolfMeme(wolf *Wolf, imgWidth int, imgHeight int, loadedDecodedJPEG image.Image) { //void function with OS output
 	//apply text
-	const fontSize = 108
+	const fontSize = 36
 	imagePath := "./wolfMeme.jpeg"
+
+	//set font face
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	face := truetype.NewFace(font, &truetype.Options{Size: fontSize})
 
 	m := gg.NewContext(imgWidth, imgHeight)
 	m.DrawImage(loadedDecodedJPEG, 0, 0)
-	m.LoadFontFace("/Library/Fonts/Impact.ttf", fontSize)
+	//m.LoadFontFace("/Library/Fonts/Impact.ttf", fontSize)
+	m.SetFontFace(face)
 
 	// Apply black stroke
 	m.SetHexColor("#000")
@@ -157,18 +169,61 @@ func getManuallySpecifiedWisdom() string { //purely void function
 	return receivedWisdom
 }
 
+//file reading functions for machine generated wisdom
+func getNumberOfLinesInText(fileName string) int {
+	// if len(os.Args) <= 1 {
+	// 	fmt.Printf("USAGE : %s <target_filename> \n", os.Args[0])
+	// 	os.Exit(0)
+	// }
+
+	// fileName := os.Args[1]
+
+	fileBytes, err := ioutil.ReadFile(fileName)
+	var numberOfLinesInText = 0
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(fileBytes), "\n")
+
+	// remove the last item from the lines slice
+	// which is empty
+	lines = lines[:len(lines)-1]
+
+	// len() function will count the total number of lines
+	fmt.Println(fileName, "has a total of", len(lines), "lines")
+	for i, line := range lines {
+		// i = i + 1 // uncomment to start from 1 instead of 0
+		fmt.Println(i, line)
+		numberOfLinesInText = numberOfLinesInText + 1
+	}
+
+	return numberOfLinesInText
+}
+
+func getMachineGeneratedWisdom() int { //string output of the machine specified wisdom
+	numberOfNouns := getNumberOfLinesInText("./Source/Noun.txt")
+
+	//var receivedWisdom = "there is error in function, please, debug"
+
+
+	return numberOfNouns
+}
+
 ////////////////////////////////////////////////////////////
 //MAIN
 
 func main() {
 	//SPECIFY WOLF NAME HERE AND DESIRED WISDOM
-	//userWisdom = "KOGDA V FORZU BLYAT?" //if you want to use default wisdom type "default"
 
 	userWisdom := getManuallySpecifiedWisdom() //get wisdom from the keyboard
 	//userWisdom = getAutomaticallySpecifiedWisdom() //neural network wisdom
+	fmt.Println(getMachineGeneratedWisdom)
 
 	newWiseWolf := newWolf("Default Wolf", userWisdom)
-	fmt.Println("Welcome to Wolf Wisdom Generator V0.0.1")
+	fmt.Println("Welcome to Wolf Wisdom Generator V0.1")
 	fmt.Println("We created the default wolf with user-specified wisdom")
 	fmt.Println("IF NO WISDOM PROVIDED WOLF SAYS DEFAULT WISDOM")
 	fmt.Println(newWiseWolf.name)

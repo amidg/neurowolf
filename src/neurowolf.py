66d829518b010640603567ca39b69de07ea660fa
Dmitrii Gusev <amidg4x4dima@gmail.com>
2022-12-26 10:39:50 -0800

    wip

--- src/neurowolf.py
+++ src/neurowolf.py
@@ -1,6 +1,10 @@
 import logging
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+# get global variables
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # basic logging
 logging.basicConfig(
@@ -14,16 +18,23 @@ logging.basicConfig(
 async def get_standard_wolf_meme(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+async def get_diffusion_wolf_meme(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, Stable Diffusion is not available yet!")
+
+'''
+extra functions
+'''
+
 # main function
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     
     # code that handles /fast_quote command that executes quote on standard image
-    get_wolf_meme_handler = CommandHandler('quote', get_standard_wolf_meme)
+    get_wolf_meme_handler = CommandHandler('std_quote', get_standard_wolf_meme)
     application.add_handler(get_wolf_meme_handler)
 
     # code that handles /diffusion command that executes quote on generated image
-    # get_wolf_meme_handler = CommandHandler('fast_quote', get_wolf_meme)
-    # application.add_handler(get_wolf_meme_handler)
+    get_wolf_meme_diff_handler = CommandHandler('diff_quote', get_diffusion_wolf_meme)
+    application.add_handler(get_wolf_meme_diff_handler)
     
     application.run_polling()
